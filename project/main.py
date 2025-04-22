2cbdfa9bfcf86495bdf815dc69c89e918646b7b1
neojarvis <techadmin@iamneo.ai>
2025-04-22 11:34:03 +0000

    Auto-save-commit-22/04/2025--17:03:59

--- project/main.py
+++ project/main.py
@@ -1,5 +1,32 @@
-import os
-from hotel_functions import load_hotels, save_hotels
+from hotel_functions import add_hotel, delete_hotel, view_hotels, update_rating, search_hotels
 
-# Define the filename for storing hotel data
-FILENAME = 'hotels.json'
+def main():
+    while True:
+        print("Menu:")
+        print("1. Add a hotel")
+        print("2. Delete a hotel")
+        print("3. Update hotel rating")
+        print("4. View all hotels")
+        print("5. Search hotels")
+        print("6. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            add_hotel()
+        elif choice == "2":
+            delete_hotel()
+        elif choice == "3":
+            update_rating()
+        elif choice == "4":
+            view_hotels()
+        elif choice == "5":
+            search_hotels()
+        elif choice == "6":
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.\n")
+
+if __name__ == "__main__":
+    main()
